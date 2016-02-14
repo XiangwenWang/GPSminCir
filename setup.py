01f0b01a05921a1419e4a69b9eac6bfc5ddcb7f0
Xiangwen Wang <wxw@vt.edu>
2016-02-14 00:43:03 -0500

    Minor changes

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup
 setup(
     name='GPSminCir',
-    packages=['GPSminCir'],
+    py_modules=["GPSminCir"],
     version='1.0.1',
     description='A tool to calculate the radius of the smallest circle that covers the given GPS track.',
     author='Xiangwen Wang',
     author_email='wangxiangwen1989@gmail.com',
     url='https://github.com/XiangwenWang/GPSminCir',
     download_url='https://github.com/XiangwenWang/GPSminCir/tarball/1.0.1',
+    license="MIT",
     keywords=['GPS', 'smallest circle'],
     classifiers=[],
 )
